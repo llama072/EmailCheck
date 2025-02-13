1d73500598f25e040284a39e13af10c23ed75439
Teó <115078440+llama072@users.noreply.github.com>
2025-02-13 13:31:43 +0100

    Add files via upload

--- Email_Tests/UnitTest1.cs
+++ Email_Tests/UnitTest1.cs
@@ -40,5 +40,91 @@ namespace Email_Tests
             Assert.IsFalse(result);
 
         }
+
+        [TestMethod]
+        public void EmailWithMultipleAtSymbols_ReturnsFalse()
+        {
+            //arranage
+
+            var validator = new Email_Validator();
+
+            //act
+
+            bool result = validator.IsValidEmail("tes@@ttest.com");
+
+            //assert
+
+            Assert.IsFalse(result);
+
+        }
+
+        [TestMethod]
+        public void EmailWithoutDomain_ReturnsFalse()
+        {
+            //arranage
+
+            var validator = new Email_Validator();
+
+            //act
+
+            bool result = validator.IsValidEmail("test@");
+
+            //assert
+
+            Assert.IsFalse(result);
+
+        }
+
+        [TestMethod]
+        public void EmailWithoutLocalPart_ReturnsFalse()
+        {
+            //arranage
+
+            var validator = new Email_Validator();
+
+            //act
+
+            bool result = validator.IsValidEmail("@example.com");
+
+            //assert
+
+            Assert.IsFalse(result);
+
+        }
+
+        [TestMethod]
+        public void EmailWithoutTLD_ReturnsFalse()
+        {
+            //arranage
+
+            var validator = new Email_Validator();
+
+            //act
+
+            bool result = validator.IsValidEmail("test@examp");
+
+            //assert
+
+            Assert.IsFalse(result);
+
+
+
+        }
+        [TestMethod]
+        public void EmptyEmail_ReturnsFalse()
+        {
+            //arranage
+
+            var validator = new Email_Validator();
+
+            //act
+
+            bool result = validator.IsValidEmail("");
+
+            //assert
+
+            Assert.IsFalse(result);
+
+        }
     }
 }
